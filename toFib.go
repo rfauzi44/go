5987e236d2db4d6d2f235a52855bfade0d5ca95a
Rizal <rizaldifauzi44@gmail.com>
2023-01-24 00:20:51 +0700

    channel

---
+++ toFib.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fibbonaci(ch chan<- []int) {
+	defer wg.Done()
+	limit := 40
+	series := numberSeriesReturn{&limit}
+	seriesFib := series.fib()
+	ch <- seriesFib
+}
+
+func evenOdd(ch <-chan []int) {
+	defer wg.Done()
+	theFib := <-ch
+	var result []int
+	for _, v := range theFib {
+		if v%2 != 0 {
+			result = append(result, v)
+		}
+	}
+	fmt.Println("ganjil", result)
+}
